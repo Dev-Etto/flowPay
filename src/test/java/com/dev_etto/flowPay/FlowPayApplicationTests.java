d553787a573d9c45f423a4050306a95f7446efcd
Dev-Etto <jhontdsmit36@gmail.com>
2025-06-15 02:59:50 -0300

    feat: add setup the application.

---
+++ src/test/java/com/dev_etto/flowPay/FlowPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev_etto.flowPay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
